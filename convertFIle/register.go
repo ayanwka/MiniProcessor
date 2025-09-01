ac7bf7aa97b98c3655ea7c40317ffe77b81f42fc
Ayan Marat <maratayan1509@gmail.com>
2025-09-01 17:33:57 +0500

    switch structure fixed

--- convertFIle/register.go
+++ convertFIle/register.go
@@ -10,68 +10,75 @@ import (
 func FileTransform(file []byte, name string) {
 	ByteToString := string(file)
 	arr := strings.Fields(ByteToString)
-
-	// проверка артиклей
 	for i := 0; i < len(arr); i++ {
-		if arr[i] == "a" && ((arr[i+1][0] == 'a') || ((arr[i+1][0] == 'e') || (arr[i+1][0] == 'i') || (arr[i+1][0] == 'o') || (arr[i+1][0] == 'u') || (arr[i+1][0] == 'h'))) {
-			arr[i] = "an"
-		}
-	}
-
-	for i := 0; i < len(arr); i++ {
-		switch arr[i] {
-		case "(up)":
-			arr[i-1] = strings.ToUpper(arr[i-1])
-			arr[i] = ""
-		case ".up":
-			num, _ := strconv.Atoi(arr[i+1])
-			for j := i - num; j < i; j++ {
-				arr[j] = strings.ToUpper(arr[j])
+		switch {
+		case strings.HasPrefix(arr[i], ("(up")):
+			if arr[i][3] == ')' {
+				arr[i-1] = strings.ToUpper(arr[i-1])
+				arr[i] = ""
+			} else {
+				numBefore := arr[i+1][0]
+				num, _ := strconv.Atoi(string(numBefore))
+				for j := i - num; j < i; j++ {
+					arr[j] = strings.ToUpper(arr[j])
+				}
+				arr[i] = ""
+				arr[i+1] = ""
 			}
-			arr[i] = ""
-			arr[i+1] = ""
-		case "(low)":
-			arr[i-1] = strings.ToLower(arr[i-1])
-			arr[i] = ""
-		case ".low":
-			num, _ := strconv.Atoi(arr[i+1])
-			for j := i - num; j < i; j++ {
-				arr[j] = strings.ToLower(arr[j])
+		case strings.HasPrefix(arr[i], ("(low")):
+			if arr[i][4] == ')' {
+				arr[i-1] = strings.ToLower(arr[i-1])
+				arr[i] = ""
+			} else {
+				numBefore := arr[i+1][0]
+				num, _ := strconv.Atoi(string(numBefore))
+				for j := i - num; j < i; j++ {
+					arr[j] = strings.ToLower(arr[j])
+				}
+				arr[i] = ""
+				arr[i+1] = ""
 			}
-			arr[i] = ""
-			arr[i+1] = ""
-		case "(cap)":
-			arr[i-1] = strings.Title(arr[i-1])
-			arr[i] = ""
-		case ".cap":
-			num, _ := strconv.Atoi(arr[i+1])
-			for j := i - num; j < i; j++ {
-				arr[j] = strings.Title(arr[j])
+		case strings.HasPrefix(arr[i], ("(cap")):
+			if arr[i][4] == ')' {
+				arr[i-1] = strings.ToTitle(arr[i-1])
+				arr[i] = ""
+			} else {
+				numBefore := arr[i+1][0]
+				num, _ := strconv.Atoi(string(numBefore))
+				for j := i - num; j < i; j++ {
+					arr[j] = strings.ToTitle(arr[j])
+				}
+				arr[i] = ""
+				arr[i+1] = ""
 			}
-			arr[i] = ""
-			arr[i+1] = ""
-		case "(hex)":
-			//ставим _, т.к convertint возвращает 2 значения. string, error
-			arr[i-1], _ = ConvertInt(arr[i-1], 16, 10)
-			arr[i] = ""
-		case ".hex":
-			num, _ := strconv.Atoi(arr[i+1])
-			for j := i - num; j < i; j++ {
-				arr[j], _ = ConvertInt(arr[j], 16, 10)
+		case strings.HasPrefix(arr[i], ("(hex")):
+			if arr[i][4] == ')' {
+				//ставим _, т.к convertint возвращает 2 значения. string, error
+				arr[i-1], _ = ConvertInt(arr[i-1], 16, 10)
+				arr[i] = ""
+			} else {
+				numBefore := arr[i+1][0]
+				num, _ := strconv.Atoi(string(numBefore))
+				for j := i - num; j < i; j++ {
+					arr[j], _ = ConvertInt(arr[j], 16, 10)
+				}
+				arr[i] = ""
+				arr[i+1] = ""
 			}
-			arr[i] = ""
-			arr[i+1] = ""
-		case "(bin)":
-			ConvertInt(arr[i-1], 2, 10)
-			arr[i-1], _ = ConvertInt(arr[i-1], 2, 10)
-			arr[i] = ""
-		case ".bin":
-			num, _ := strconv.Atoi(arr[i+1])
-			for j := i - num; j < i; j++ {
-				arr[j], _ = ConvertInt(arr[j], 2, 10)
+		case strings.HasPrefix(arr[i], ("(bin")):
+			if arr[i][4] == ')' {
+				ConvertInt(arr[i-1], 2, 10)
+				arr[i-1], _ = ConvertInt(arr[i-1], 2, 10)
+				arr[i] = ""
+			} else {
+				numBefore := arr[i+1][0]
+				num, _ := strconv.Atoi(string(numBefore))
+				for j := i - num; j < i; j++ {
+					arr[j], _ = ConvertInt(arr[j], 2, 10)
+				}
+				arr[i] = ""
+				arr[i+1] = ""
 			}
-			arr[i] = ""
-			arr[i+1] = ""
 		}
 	}
 
