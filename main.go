56fbc46c81477f379d9e634e2632c4900c1df1ee
Ayan Marat <maratayan1509@gmail.com>
2025-09-01 16:04:27 +0500

    articles in 2nd branch

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	Convert "test_game1/convertFIle"
 )
 
 func main() {
+	// os.Args принимает аргументы прямо из консоли
+	// go run . input.txt output.txt
 	file1 := os.Args[1]
 	file2 := os.Args[2]
+	// 3, потому что os.Args всегда включает имя программы как первый элемент
+	if len(os.Args) != 3 {
+		fmt.Println("enter: go run . file_1 file_2")
+		return
+	}
 	Convert.WriteFile(string(file1), string(file2))
 }
