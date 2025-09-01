1da763ff41526bb70025897c5cb3ac0d933b6e75
Ayan Marat <maratayan1509@gmail.com>
2025-09-01 18:56:42 +0500

    add Checking for err + input can read 10+ elements

--- convertFIle/register.go
+++ convertFIle/register.go
@@ -25,8 +25,14 @@ func FileTransform(file []byte, name string) {
 				arr[i-1] = strings.ToUpper(arr[i-1])
 				arr[i] = ""
 			} else {
-				numBefore := arr[i+1][0]
+				numBefore := strings.TrimSuffix(strings.TrimPrefix(arr[i+1], "(up,"), ")")
+				numBefore = strings.TrimSpace(numBefore)
 				num, _ := strconv.Atoi(string(numBefore))
+				// проверка ошибки в файле input.txt
+				if i-num < 0 {
+					fmt.Println("index IN FILE IS out of range")
+					return
+				}
 				for j := i - num; j < i; j++ {
 					arr[j] = strings.ToUpper(arr[j])
 				}
@@ -38,8 +44,13 @@ func FileTransform(file []byte, name string) {
 				arr[i-1] = strings.ToLower(arr[i-1])
 				arr[i] = ""
 			} else {
-				numBefore := arr[i+1][0]
+				numBefore := strings.TrimSuffix(strings.TrimPrefix(arr[i+1], "(low,"), ")")
+				numBefore = strings.TrimSpace(numBefore)
 				num, _ := strconv.Atoi(string(numBefore))
+				if i-num < 0 {
+					fmt.Println("index IN FILE IS out of range")
+					return
+				}
 				for j := i - num; j < i; j++ {
 					arr[j] = strings.ToLower(arr[j])
 				}
@@ -48,13 +59,18 @@ func FileTransform(file []byte, name string) {
 			}
 		case strings.HasPrefix(arr[i], ("(cap")):
 			if arr[i][4] == ')' {
-				arr[i-1] = strings.ToTitle(arr[i-1])
+				arr[i-1] = strings.Title(arr[i-1])
 				arr[i] = ""
 			} else {
-				numBefore := arr[i+1][0]
+				numBefore := strings.TrimSuffix(strings.TrimPrefix(arr[i+1], "(cap,"), ")")
+				numBefore = strings.TrimSpace(numBefore)
 				num, _ := strconv.Atoi(string(numBefore))
+				if i-num < 0 {
+					fmt.Println("index IN FILE IS out of range")
+					return
+				}
 				for j := i - num; j < i; j++ {
-					arr[j] = strings.ToTitle(arr[j])
+					arr[j] = strings.Title(arr[j])
 				}
 				arr[i] = ""
 				arr[i+1] = ""
@@ -65,8 +81,13 @@ func FileTransform(file []byte, name string) {
 				arr[i-1], _ = ConvertInt(arr[i-1], 16, 10)
 				arr[i] = ""
 			} else {
-				numBefore := arr[i+1][0]
+				numBefore := strings.TrimSuffix(strings.TrimPrefix(arr[i+1], "(hex,"), ")")
+				numBefore = strings.TrimSpace(numBefore)
 				num, _ := strconv.Atoi(string(numBefore))
+				if i-num < 0 {
+					fmt.Println("index IN FILE IS out of range")
+					return
+				}
 				for j := i - num; j < i; j++ {
 					arr[j], _ = ConvertInt(arr[j], 16, 10)
 				}
@@ -79,8 +100,13 @@ func FileTransform(file []byte, name string) {
 				arr[i-1], _ = ConvertInt(arr[i-1], 2, 10)
 				arr[i] = ""
 			} else {
-				numBefore := arr[i+1][0]
+				numBefore := strings.TrimSuffix(strings.TrimPrefix(arr[i+1], "(bin,"), ")")
+				numBefore = strings.TrimSpace(numBefore)
 				num, _ := strconv.Atoi(string(numBefore))
+				if i-num < 0 {
+					fmt.Println("index IN FILE IS out of range")
+					return
+				}
 				for j := i - num; j < i; j++ {
 					arr[j], _ = ConvertInt(arr[j], 2, 10)
 				}
